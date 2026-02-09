3accb1cc8fff9b2b686e5162276408bca733a66a
kasyifana <muhammadli1009@gmail.com>
2026-02-09 21:33:07 +0700

    css

--- config.js
+++
@@ -1,7 +0,0 @@
-// Konfigurasi environment variables
-// Buat file .env di root folder untuk production
-
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-};
