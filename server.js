da510a9a59bdd3bff3e79f0f5cb53aa57330a95c
kasyifana <muhammadli1009@gmail.com>
2026-02-09 21:35:44 +0700

    s

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files
-app.use('/css', express.static(path.join(__dirname, 'public/css')));
+// Serve static files (JavaScript only, CSS is inline in EJS)
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
 
 // Middleware
